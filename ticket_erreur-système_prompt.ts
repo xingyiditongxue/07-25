c31e6cb81618871869e4a832cf3d65ddeac59cee
xingyiditongxue <xingyiditongxue@gmail.com>
2025-07-16 14:12:55 +0200

    Update ticket_erreur-système_prompt.ts

--- ticket_erreur-système_prompt.ts
+++ ticket_erreur-système_prompt.ts
@@ -1,4 +1,4 @@
-2771-Refonte-des-messages-d-erreur-systeme
+2771-Refonte-des-messages-d-erreur-systeme  ; oxaflow/2771-Refonte-des-messages-d-erreur-systeme_20250716_122732
 ----------------------------------------------------------------------------------------------
 Dans mon projet il y a plusieurs endroits où l'on lève un OxaErrorCode.GENERIC_ERROR, I would like to replace all of these generic_error by other specific error type; Each time tht there is not a specific error type appropriate in oxaerrorcode.enum, we would create one.
 And for every newly created oxaerror type, we would like to introduce it into en-GB.json, fr-FR.json to make the i18n message.
