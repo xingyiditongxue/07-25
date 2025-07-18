803fbb6db6263428f32f6678119936570b2333d9
xingyiditongxue <xingyiditongxue@gmail.com>
2025-07-18 11:31:31 +0200

    Create ticket_erreur-système_oxausineV8_1.ts

---
+++ ticket_erreur-système_oxausineV8_1.ts
@@ -0,0 +1,7 @@
+2771-Refonte-des-messages-d-erreur-systeme
+-------------------------------------------------------------------------
+Dans src/background/controllers/Autoxa.controller.ts, j'ai 2 fonctions: 'sendMessageToUserFromAutoxa' et 'askQuestionTextToUserFromAutoxa'. 
+
+Je voudrais d'abord être expliqué en détail comment je peux produire la "AUTOXA_MESSAGE_TO_USER_FAILED" erreur avec ces 2 fonctions. Veuillez rajouter cette explication en tant que commentaires sur le fichier "src/background/controllers/Autoxa.controller.ts"
+ 
+En suite je voudrais avoir les tests unitaires qui vérifient "AUTOXA_MESSAGE_TO_USER_FAILED" erreur s'affichent correctement
