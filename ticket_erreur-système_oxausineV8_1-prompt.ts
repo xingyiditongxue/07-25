2cdfc5f11b84fc7ef1ec3109af618bee723b9b45
xingyiditongxue <xingyiditongxue@gmail.com>
2025-07-18 11:40:01 +0200

    Update ticket_erreur-système_oxausineV8_1-prompt.ts

--- ticket_erreur-système_oxausineV8_1-prompt.ts
+++ ticket_erreur-système_oxausineV8_1-prompt.ts
@@ -4,4 +4,4 @@ Dans src/background/controllers/Autoxa.controller.ts, j'ai 2 fonctions: 'sendMes
 
 Je voudrais d'abord être expliqué en détail comment je peux produire la "AUTOXA_MESSAGE_TO_USER_FAILED" erreur avec ces 2 fonctions. Veuillez rajouter cette explication en tant que commentaires sur le fichier "src/background/controllers/Autoxa.controller.ts"
  
-En suite je voudrais avoir les tests unitaires qui vérifient "AUTOXA_MESSAGE_TO_USER_FAILED" erreur s'affichent correctement
+En suite je voudrais avoir les tests unitaires qui vérifient "AUTOXA_MESSAGE_TO_USER_FAILED" erreur s'affichent correctement avec ces 2 fonctions: `sendMessageToUserFromAutoxa` et `askQuestionTextToUserFromAutoxa`
